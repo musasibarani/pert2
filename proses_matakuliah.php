8d19ee8293b6bf2a3943e00151684d61543e24a2
Musa Sibarani <32220501+musasibarani@users.noreply.github.com>
2017-10-05 17:38:58 +0100

    Matkul (Create, read, update)

---
+++ proses_matakuliah.php
@@ -0,0 +1,19 @@
+<?php //filename: proses_mhs.php
+	include ("db.php");
+
+	if($_GET['action'] == "add")
+	{
+		//2. Query
+		$query ="INSERT INTO matakuliah (kode_matkul, nama) VALUES ('$_POST[kode_matkul]', '$_POST[nama]')";
+	}
+	else if($_GET['action'] == "edit")
+	{
+		//2.Query
+		$query = "UPDATE matakuliah SET kode_matkul = '$_POST[kode_matkul]', nama = '$_POST[nama]' WHERE id=$_POST[id]";
+	}
+
+	mysqli_query ($koneksi, $query);
+	
+	//REDIRECT ke template.php
+	header ('Location: template.php?page=matakuliah');
+?>
