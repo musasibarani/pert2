32237601dcbfe83d564cdf3a69942ffd75d619d8
Musa Sibarani <32220501+musasibarani@users.noreply.github.com>
2017-10-05 17:36:27 +0100

    Matkul (Create)

--- template.php
+++ template.php
@@ -7,16 +7,17 @@
 	<head>
 		<title></title>
 		<style>
-			#header{background: blue;}
-			#sidebar{background: red;}
-			#footer{background: orange;}
+			#header{background: yellow;}
+			#sidebar{background: orange;}
+			#footer{background: green;}
 		</style>
 	</head>
 	<body>
 		<div id="header"><h3>Sistem Informasi Kampus</h3></div>
 		<div id="sidebar">
 			<a href="template.php?page=mahasiswa">Mahasiswa</a>&nbsp|
-			<a href="template.php?page=dosen">Dosen</a>
+			<a href="template.php?page=dosen">Dosen</a> |
+			<a href="template.php?page=matakuliah">Mata Kuiah</a>
 		</div>
 		<div id="content">
 			Ini adalah konten
